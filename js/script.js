04d94f8bf29b640a3092e79d16940974f6f88643
Chris Robbins <Jaguar85>
2016-08-29 15:21:52 -0500

    auth0 foundation work

---
+++ js/script.js
@@ -0,0 +1,85 @@
+var lock = new Auth0Lock(
+  'kVq2br5crIamzToIbqQnGIY6anIO33cY',
+  'seesharp.auth0.com'
+);
+// Listening for the authenticated event
+lock.on("authenticated", function(authResult) {
+  // Use the token in authResult to getProfile() and save it to localStorage
+  lock.getProfile(authResult.idToken, function(error, profile) {
+    if (error) {
+      // Handle error
+      return;
+    }
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+      loadToDoApp()
+  });
+});
+ $(document).ready(function () {
+  $('#login-button').click(function (e) {
+    e.preventDefault()
+    lock.show()
+  })
+  $('#logout-button').click(function(e) {
+    e.preventDefault()
+    logOut()
+  })
+});
+
+function logOut() {
+  localStorage.removeItem('id_token')
+  showWelcome()
+}
+
+function checkLoggedIn() {
+  if (isLoggedIn()) {
+    loadToDoApp()
+  }else {
+    showWelcome()
+  }
+}
+function showWelcome() {
+  $('#welcome').show()
+  $('#app').hide()
+}
+
+function isLoggedIn() {
+  var idToken = localStorage.getItem('id_token')
+  if (idToken) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function loadToDoApp() {
+  console.log('loadFeedApp');
+  showApp()
+}
+
+// function loadTodo(todo) {
+//   var li = $('<li></li>');
+//     li.text(todo.task + ' ');
+//     var a = $('<a>Delete</a>');      
+//     a.attr('href','http://localhost:3000/bands/' + todo._id);     a.addClass('deleteBand');    
+//     li.append(a);    
+//      $('#todos').append(li);
+//      console.log(todo);
+//
+// }
+// function deleteToDo() {
+//   $(document).on('click', 'a.deleteToDo', function (e) {
+//     e.preventDefault()
+//     var link = $(this)
+//     $.ajax({
+//       url: link.attr('href'),
+//       method: 'DELETE'
+//     })
+//     .done(function () {
+//       link.parent('li').remove()
+//     })
+//   })
+// }
+function showApp() {
+  $('#app').show()
+  $('#welcome').hide()
+}
