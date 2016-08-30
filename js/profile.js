1016fce9421e3399071473231f432e95db9c116f
Chris Robbins <Jaguar85>
2016-08-30 11:24:38 -0500

    linked js to html

--- js/profile.js
+++ js/profile.js
@@ -6,22 +6,17 @@ getProfile();
 });
 
 function createProfile() {
-  $('.<submit-btn>').on('submit', function(e) {
+
+  $('#contact').on('submit', function(e) {
     e.preventDefault()
-    var newProfile = [];
-    newProfile.push($('#fullName-field').val());
-    newProfile.push($('#businessType-field').val());
-    newProfile.push($('#address-field').val());
-    newProfile.push($('#phone-field').val());
-    newProfile.push($('#email-field').val());
-    console.log();
-    $.ajax({
-      url: 'http://localhost:3000/profiles',
-      method: 'POST',
-      data: $('.profile-form').serialize()
-    }).done(function (newProfile) {
-      (newProfile).val('')
-    })
+    console.log($('#contact').serialize());
+    // $.ajax({
+    //   url: 'http://localhost:3000/profiles',
+    //   method: 'POST',
+    //   data: $('.profile-form').serialize()
+    // }).done(function (newProfile) {
+    //   (newProfile).val('')
+    // })
   })
 }
 
