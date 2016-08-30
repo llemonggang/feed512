49137513b6b7d73a9a50dcf405f23eee0926fe7f
Chris Robbins <Jaguar85>
2016-08-30 11:30:35 -0500

    changed stylesheet hrefs

--- js/profile.js
+++ js/profile.js
@@ -9,14 +9,14 @@ function createProfile() {
 
   $('#contact').on('submit', function(e) {
     e.preventDefault()
-    console.log($('#contact').serialize());
-    // $.ajax({
-    //   url: 'http://localhost:3000/profiles',
-    //   method: 'POST',
-    //   data: $('.profile-form').serialize()
-    // }).done(function (newProfile) {
-    //   (newProfile).val('')
-    // })
+    // console.log($('#contact').serialize());
+    $.ajax({
+      url: 'http://localhost:3000/profiles',
+      method: 'POST',
+      data: $('.profile-form').serialize()
+    }).done(function (newProfile) {
+      (newProfile).val('')
+    })
   })
 }
 
