0653e618f5f4b01fd12de52b3af1633921376594
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-08-30 14:43:21 -0500

    push to inbox

--- js/inbox.js
+++ js/inbox.js
@@ -4,26 +4,30 @@ pushInbox()
 
 function pushInbox() {
 
-$('<.don-submit>').on('click', function(e) {
+$('.don-submit').on('click', function(e) {
   e.preventDefault()
   $.ajax({
-    url: 'http://localhost:3000/donations',
-    method: 'POST',
-    data: $('.don-form').serialize()
-    var li = $('<li/>')
-    li.text(donation.type + ' ')
-    li.append(a)
-    $('#don-confirm').append(li)
-    claimItems();
-  }).done(function (hideDonation) {
-    (hideDonation).val('')
+    url: 'http://localhost:3000/donations/claimed',
+    method: 'GET'
+  }).done(function (donations) {
+    donations.forEach(function (donation) {
+      loadDoation(donation)
+
+    })
+    // (hideDonation).val('')
   })
 })
 }
 
-function claimItems() {
-  url: 'http://localhost:3000/profiles',
-  method: 'GET',
-  data: userId
+function loadDoation(donation) {
+  var li = $('<li/>')
+  li.text(donation.type + ' ')
 
+  $.ajax({
+    url: '/profiles/' + donation.recipientId
+  }).done(function (profile) {
+    li.append(profile.fullName)
+  })
+
+  $('#don-confirm').append(li)
 }
