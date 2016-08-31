2f3ea0f82081a59e909dc6605d92306c34faf6d3
niran manandhar <nirnaism@gmail.com>
2016-08-30 20:32:54 -0500

    killin it huh guys

--- js/matches.js
+++ js/matches.js
@@ -1,19 +1,19 @@
-function toggleClaim() {
+function togClaimed() {
   $(document).on('click', 'li', function (e) {
     e.preventDefault()
 
     // this is the link that was clicked
     var li = $(this)
-
-    $.ajax({
-      url: js/,
-      method: 'PUT',
-      data: {
-        corruptedByTheSystem: !li.hasClass('corrupted')
-      }
-    })
-    .done(function () {
-      li.toggleClass('corrupted')
-    })
   })
 }
+
+$(document).ready(function() {
+  $.ajax({
+    url: 'js/test-matches.json',
+    method: 'GET'
+  })
+  .done(function (data) {
+    // li.toggleClass('claimed')
+    console.log(data);
+  })
+});
