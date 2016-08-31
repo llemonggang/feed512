75da5380cbab63a53ede0702682b4ce0c14138dc
niran manandhar <nirnaism@gmail.com>
2016-08-31 12:27:08 -0500

    deleted hardcodes li

--- js/matches.js
+++ js/matches.js
@@ -1,20 +1,20 @@
-function Claimed() {
-  $(document).on('click', 'li', function (e) {
-    e.preventDefault()
-
-    // this is the link that was clicked
-    var li = $(this)
-  })
-}
+// function Claimed() {
+//   $(document).on('click', 'li', function (e) {
+//     e.preventDefault()
+//
+//     // this is the link that was clicked
+//     var li = $(this)
+//   })
+// }
 
 $(document).ready(function() {
   $.ajax({
-    url: 'js/test-matches.json',
+    url: '/test-matches.json',
     //this will be replaced by the actual url. just checking
     method: 'GET'
   })
   .done(function (data) {
     // li.toggleClass('claimed')
-    console.log(data);
+    console.log(res.JSON);
   })
 });
