1cec1c657f98470ec55d89b62289d8888705abb7
niran manandhar <nirnaism@gmail.com>
2016-09-01 11:52:36 -0500

    added new function and foreach

--- js/inbox.js
+++ js/inbox.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-pushInbox()
+// pushInbox()
+loadInbox()
 });
 
-function pushInbox() {
-
-$('.don-submit').on('click', function(e) {
-  e.preventDefault()
+function loadInbox() {
   $.ajax({
     headers: {
       'Authorization': 'Bearer ' + localStorage.getItem('id_token')
@@ -14,26 +12,31 @@ $('.don-submit').on('click', function(e) {
     method: 'GET'
   }).done(function (donations) {
     donations.forEach(function (donation) {
-      loadDoation(donation)
-
-    })
-    // (hideDonation).val('')
-  })
-})
-}
-
-function loadDoation(donation) {
-  var li = $('<li/>')
-  li.text(donation.type + ' ')
-
-  $.ajax({
-    url: '/profiles/' + donation.recipientId
-    headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('id_token')
-    },
-  }).done(function (profile) {
-    li.append(profile.fullName)
+      var li = $('<li />')
+      li.text(donation.type)
+      $('#don-confirm').append(li)
+      console.log(donations);
+      })
   })
 
-  $('#don-confirm').append(li)
 }
+
+// function pushInbox() {
+//
+// $('.don-submit').on('click', function(e) {
+//   e.preventDefault()
+//   $.ajax({
+//     headers: {
+//       'Authorization': 'Bearer ' + localStorage.getItem('id_token')
+//     },
+//     url: 'http://localhost:3000/donations/claimed',
+//     method: 'GET'
+//   }).done(function (donations) {
+//     donations.forEach(function (donation) {
+//       loadDoation(donation)
+//
+//     })
+//     // (hideDonation).val('')
+//   })
+// })
+// }
